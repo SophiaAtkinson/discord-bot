f213036127360b1ed009a3121c2535a97c59def6
Jim Sim <ethan97062@gmail.com>
2020-08-20 00:02:05 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+    
+client.once('ready', () => {
+    console.log('Ready')
+
+    client.user.setActivity("$help", {
+        type: "STREAMING",
+        url: "https://www.twitch.tv/user"
+    });
+})
+client.on('message', message => {
+    console.log(message.content);
+    //Help Cmd
+    if (message.content.startsWith(`${prefix}test`))
+        message.channel.send("This test works")
+
+})
+
+
+
+client.login(token);
