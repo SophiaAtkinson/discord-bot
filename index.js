257f8f28ded7a7f1be61b6acb5c02fd4532a53db
Jim Sim <ethan97062@gmail.com>
2020-10-05 19:28:22 -0700

    Added kick/ ban commands

--- index.js
+++ index.js
@@ -15,6 +15,52 @@ client.on('message', message => {
 
     if (message.content.startsWith(`${prefix}test`))
         message.channel.send("This test works")
+    
+            //Kick
+        if (message.content.startsWith(`${prefix}kick`)) {
+            const user = message.mentions.users.first();
+            if (user) {
+              const member = message.guild.member(user);
+              if (member) {
+                member
+                  .kick('Optional reason that will display in the audit logs')
+                  .then(() => {
+                    message.reply(`Successfully kicked ${user.tag}`);
+                  })
+                  .catch(err => {
+                    message.reply('I was unable to kick the member');
+                    console.error(err);
+                  });
+              } else {
+                message.reply("That user isn't in this guild!");
+              }
+            } else {
+              message.reply("You didn't mention the user to kick!");
+            }
+          }
+        //Kick End
+        //Ban
+        if (message.content.startsWith(`${prefix}ban`)) {
+            const user = message.mentions.users.first();
+            if (user) {
+              const member = message.guild.member(user);
+              if (member) {
+                member
+                  .ban('Optional reason that will display in the audit logs')
+                  .then(() => {
+                    message.reply(`Successfully Banned ${user.tag}`);
+                  })
+                  .catch(err => {
+                    message.reply('I was unable to Ban the member');
+                    console.error(err);
+                  });
+              } else {
+                message.reply("That user isn't in this guild!");
+              }
+            } else {
+              message.reply("You didn't mention the user to Ban!");
+            }
+          }
 
 })
 
